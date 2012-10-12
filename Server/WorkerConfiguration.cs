0cea448dea6ce37abd3f5c1e5e723f7a62752b69
Björn Johansson <bjn.johansson@gmail.com>
2012-10-12 20:21:07 +0200

    Disabled second level retries

---
+++ Server/WorkerConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using NServiceBus;
+using NServiceBus.Config;
+
+namespace Server
+{
+	[EndpointName("sample.server")]	
+	public class WorkerConfiguration : IConfigureThisEndpoint, AsA_Publisher, INeedInitialization
+	{
+		public void Init()
+		{
+			Configure.Instance.DisableSecondLevelRetries();
+		}
+	}
+}
